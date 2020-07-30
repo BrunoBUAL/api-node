330b3429a8b3e0959cbec9283d9925dde89fc289
Bruno <brunobual@edu.unisinos.br>
2020-07-30 17:34:06 -0300

    Meu segundo commit

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 let port = 3000
 app.listen(port,() => {
-    console.log('Servidor rodando na porta ${port}')
+    console.log(`Servidor rodando na porta ${port} `)
 })
 
+app.use(bodyParser.json())
+
+const users = {
+
+}
+
+//trazer informação
 app.get('/',(req, res) =>{
-    res.send("minha primeira rota")
+    res.json({users})
+})
+//criar
+app.post('/',(req, res) =>{
+   const {nome, cidade, idade} = req.body
+
+   users[nome] = {cidade, idade, nome}
+   
+    //console.log(req.body)
+    res.json({msg:"Usuario criado com sucesso!"})
 })
 
-app.get('/teste',(req, res) =>{
-    res.send("meu primeiro teste")
+//users["Bruno"] = {idade:25, cidade:"RS"}
+
+
+//atualizar
+app.put('/:nome',(req, res) =>{
+    const {nome} = req.params
+    const {cidade, idade} = req.body
+
+    users[nome] = {cidade, idade, nome}
+
+    //console.log(nome, cidade, idade)
+    res.json("Usuario Alterado")
+})
+//deletar
+app.delete('/:nome',(req, res) =>{
+    const {nome} = req.params;
+    delete users[nome]
+    res.json("Usuario deletado")
 })
 
 
